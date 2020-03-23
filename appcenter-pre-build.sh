d7164406f7415c6b928411308455f2b5c19894d6
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-03-23 14:26:23 +0300

    add new brew

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -5,3 +5,4 @@ export JAVA_HOME=$(/usr/libexec/java_home -v11)
 echo "changed java version to 1.11"
 java -version
 echo "export JAVA_HOME=$(/usr/libexec/java_home -v11)" >> "${HOME}/.bashrc"
+brew install homebrew/cask/java
